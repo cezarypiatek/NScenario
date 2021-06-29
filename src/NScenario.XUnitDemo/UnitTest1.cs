38261933d192323557a84f91f36767f4040d9a9b
Sergiy Osypchuk <osypchuk@ssocialtalents.com>
2021-06-28 21:53:38 -0700

    Example updated

--- src/NScenario.XUnitDemo/UnitTest1.cs
+++ src/NScenario.XUnitDemo/UnitTest1.cs
@@ -6,28 +6,38 @@ namespace NScenario.XUnitDemo
 {
     public class UnitTest1
     {
+        private Action _AssertAction = () =>
+        {
+            Assert.True(false);
+        };
+
         [Fact]
-        public async Task Assert_which_fails_as_expected()
+        public async Task Assert_in_step_action_not_throwing_exception()
         {
             var scenario = TestScenarioFactory.Default();
-            // Works perfect
-            await scenario.Step("I am assert which fails", async () =>
-            {
-                Assert.True(false);
+            await scenario.Step("I am assert which should fail", _AssertAction);
+        }
+
+        [Fact]
+        public async Task Assert_in_step_action_throws_an_exception_when_async_used()
+        {
+            var scenario = TestScenarioFactory.Default();
+            await scenario.Step("I am assert which should fail", async () => { 
+                // CS1998 Warning here but it fails
+                _AssertAction(); 
             });
         }
 
         [Fact]
-        public async Task Assert_Which_is_not_throwing_exception()
+        public async Task Plain_action_fails_in_async_test_method()
         {
-            var scenario = TestScenarioFactory.Default();
-            // Actual Result:
-            // Test succeed
-            // Expected result: Test failure
-            await scenario.Step("I am assert which fails", () =>
-            {
-                Assert.True(false);
-            });
+            _AssertAction();
+        }
+
+        [Fact]
+        public void Plain_action_fails_in_regular_test_method()
+        {
+            _AssertAction();
         }
     }
 }
