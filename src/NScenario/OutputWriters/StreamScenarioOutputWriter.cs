2b005764f0c4d038964a439c97204c367440c81a
Cezary Piątek <cezarypiatek@live.com>
2022-03-15 19:53:39 +0100

    #10: Add option for exporting scenario transcription as Markdown and HTML

---
+++ src/NScenario/OutputWriters/StreamScenarioOutputWriter.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using NScenario.StepExecutors;
+
+namespace NScenario.OutputWriters
+{
+    public class StreamScenarioOutputWriter : IScenarioOutputWriter
+    {
+        private readonly TextWriter _outputWriter;
+
+        public StreamScenarioOutputWriter(TextWriter outputWriter)
+        {
+            _outputWriter = outputWriter;
+        }
+
+        public void WriteStepDescription(string description)
+        {
+            _outputWriter.WriteLine(description);
+        }
+
+        public void WriteScenarioTitle(string scenarioTitle)
+        {
+            _outputWriter.WriteLine(scenarioTitle);
+        }
+    }
+}
--- src/NScenario/TestScenarioFactory.cs
+++ src/NScenario/TestScenarioFactory.cs
@@ -1,21 +1,26 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Runtime.CompilerServices;
+using NScenario.OutputWriters;
 using NScenario.StepExecutors;
 
 namespace NScenario
 {
     public static class TestScenarioFactory
     {
+        public static IScenarioOutputWriter DefaultScenarioOutputWriter { get; set; } = new StreamScenarioOutputWriter(Console.Out);
+
         public static ITestScenario Default(TextWriter outputWriter = null, string scenarioPrefix = null, string stepPrefix = null, [CallerMemberName] string testMethodName = "")
         {
-            var stepExecutor = BuildScenarioStepExecutor(outputWriter, scenarioPrefix, stepPrefix);
+            var selectedOutputWriter = outputWriter != null ? new StreamScenarioOutputWriter(outputWriter) : DefaultScenarioOutputWriter;
+            var stepExecutor = BuildScenarioStepExecutor(selectedOutputWriter, scenarioPrefix, stepPrefix);
             return new TestScenario(stepExecutor, testMethodName);
         }
 
-        private static IScenarioStepExecutor BuildScenarioStepExecutor(TextWriter outputWriter,string scenarioPrefix = null, string stepPrefix = null)
+        private static IScenarioStepExecutor BuildScenarioStepExecutor(IScenarioOutputWriter scenarioOutputWriter, string scenarioPrefix = null, string stepPrefix = null)
         {
             var stepExecutorBuilder = new DecoratorBuilder<IScenarioStepExecutor>();
-            stepExecutorBuilder.WrapWith(_ => new OutputScenarioStepExecutor(outputWriter));
+            stepExecutorBuilder.WrapWith(_ => new OutputScenarioStepExecutor(scenarioOutputWriter));
             stepExecutorBuilder.WrapWith(u => new IndentionScenarioStepExecutor(u));
             stepExecutorBuilder.WrapWith(u => new PrefixedScenarioStepExecutor(u, scenarioPrefix, stepPrefix));
             stepExecutorBuilder.WrapWith(u => new OrderedScenarioStepExecutor(u));
