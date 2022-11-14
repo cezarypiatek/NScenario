355dedd301fd265b09e6de952721ed7b09d49e7d
Cezary Piątek <cezarypiatek@live.com>
2022-11-14 23:04:25 +0100

    Add support for parallel exectution to MarkdownFormatterOutputWriter

--- src/NScenario/OutputWriters/MarkdownFormatterOutputWriter.cs
+++ src/NScenario/OutputWriters/MarkdownFormatterOutputWriter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using MarkdownSharp;
@@ -8,13 +9,34 @@ namespace NScenario.OutputWriters
 {
     public class MarkdownFormatterOutputWriter: IScenarioOutputWriter
     {
-        private readonly StringBuilder _outputBuilder = new StringBuilder();
+        private readonly Func<string> _currentTestIdentifierAccessor;
+        private readonly Dictionary<string, StringBuilder> _outputBuilders = new Dictionary<string, StringBuilder>();
 
-        public MarkdownFormatterOutputWriter(string? title = null)
+        private StringBuilder OutputBuilder
         {
+            get
+            {
+                var testId = _currentTestIdentifierAccessor();
+                if (_outputBuilders.TryGetValue(testId, out var builder))
+                {
+                    return builder;
+                }
+
+                return _outputBuilders[testId] = new StringBuilder();
+            }
+        }
+        
+        /// <summary>
+        ///     Export test scenario transcript to markdown report
+        /// </summary>
+        /// <param name="title">Report title</param>
+        /// <param name="currentTestIdentifierAccessor">Function that returns unique identifier representing currently executed scenario. Should be provided when tests are executed in parallel mode</param>
+        public MarkdownFormatterOutputWriter(string? title = null, Func<string>? currentTestIdentifierAccessor = null)
+        {
+            _currentTestIdentifierAccessor = currentTestIdentifierAccessor ?? (() => "SHARED_ID");
             if (title != null)
             {
-                _outputBuilder.AppendLine($"# {title}");
+                OutputBuilder.AppendLine($"# {title}");
             }
         }
 
@@ -24,27 +46,35 @@ namespace NScenario.OutputWriters
             if (prefixPosition >= 0)
             {
                 var newDescription = description.Insert(prefixPosition, "* ");
-                _outputBuilder.AppendLine(newDescription);
+                OutputBuilder.AppendLine(newDescription);
             }
             else
             {
-                _outputBuilder.AppendLine("* " + description);
+                OutputBuilder.AppendLine("* " + description);
             }
         }
 
         public void WriteScenarioTitle(string scenarioTitle)
         {
-            _outputBuilder.AppendLine($"\r\n## {scenarioTitle}\r\n");
+            OutputBuilder.AppendLine($"\r\n## {scenarioTitle}\r\n");
         }
 
-        public string GetMarkdown() => _outputBuilder.ToString();
+        public string GetMarkdown()
+        {
+            var mergedMarkdown = new StringBuilder();
+            foreach (var builder in _outputBuilders.Values)
+            {
+                mergedMarkdown.Append(builder.ToString());
+            }
+            return mergedMarkdown.ToString();
+        }
 
-        public void Save(string path) =>  File.WriteAllText(path, _outputBuilder.ToString(), Encoding.UTF8);
+        public void Save(string path) =>  File.WriteAllText(path, GetMarkdown(), Encoding.UTF8);
 
 
         public void ExportToHtml(string path)
         {
-            var markdownContent = _outputBuilder.ToString();
+            var markdownContent = GetMarkdown();
             var mm = new Markdown();
             var htmlContent = mm.Transform(markdownContent);
             File.WriteAllText(path, $"<html><head></head><body>{htmlContent}<body></html>", Encoding.UTF8);
