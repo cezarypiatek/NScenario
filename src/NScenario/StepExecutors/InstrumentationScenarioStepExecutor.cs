face624e9ac575a5467692bd2cb4d318ff48c205
Cezary Piatek <cezarypiatek@live.com>
2024-11-06 22:23:52 +0100

    Extend step activity with information about exception

--- src/NScenario/StepExecutors/InstrumentationScenarioStepExecutor.cs
+++ src/NScenario/StepExecutors/InstrumentationScenarioStepExecutor.cs
@@ -22,12 +22,44 @@ public class InstrumentationScenarioStepExecutor : IScenarioStepExecutor
     public async Task Step(string scenarioName, string stepDescription, Func<Task> action, StepContext stepContext)
     {
         using var activity = NScenarioInstrumentation.DiagnosticSource.StartActivity(name: stepDescription);
-        await _scenarioStepExecutorImplementation.Step(scenarioName, stepDescription, action, stepContext);
+        try
+        {
+            await _scenarioStepExecutorImplementation.Step(scenarioName, stepDescription, action, stepContext);
+        }
+        catch (Exception e)
+        {
+            TryToMarkActivityAsFailed(activity, e);
+            throw;
+        }
+    }
+
+    private static void TryToMarkActivityAsFailed(Activity? activity, Exception e)
+    {
+        if (activity != null)
+        {
+            activity.AddEvent(new ActivityEvent("exception",tags: new ActivityTagsCollection
+            {
+                ["operation.status"] = "failed",
+                ["exception.escaped"] = true,
+                ["exception.type"] = e.GetType().ToString(),
+                ["exception.message"] = e.Message,
+                ["exception.stacktrace"] = e.StackTrace
+            }));
+            activity.SetStatus(ActivityStatusCode.Error, e.Message);
+        }
     }
 
     public async Task Step(string scenarioName, string stepDescription, Action action, StepContext stepContext)
     {
         using var activity = NScenarioInstrumentation.DiagnosticSource.StartActivity(name: stepDescription);
-        await _scenarioStepExecutorImplementation.Step(scenarioName, stepDescription, action, stepContext);
+        try
+        {
+            await _scenarioStepExecutorImplementation.Step(scenarioName, stepDescription, action, stepContext);
+        }
+        catch (Exception e)
+        {
+            TryToMarkActivityAsFailed(activity, e);
+            throw;
+        }
     }
 }
